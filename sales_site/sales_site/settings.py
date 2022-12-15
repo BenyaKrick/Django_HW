fe2f70a99fbb1d2147262ec151f440a8f635d804
benyakrck <sergrb27@yandex.ru>
2022-12-15 20:50:47 +0300

    database settings

--- sales_site/sales_site/settings.py
+++ sales_site/sales_site/settings.py
@@ -60,12 +60,12 @@ WSGI_APPLICATION = 'sales_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -111,6 +111,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 try:
     from .local_settings import *
 except ImportError:
